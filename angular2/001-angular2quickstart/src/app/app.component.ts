8aff7e81ca5af19c565ccbb0c2b8276fab646ada
Pruthvi Kalidindi <Pruthvi.Kalidindi@infor.com>
2019-12-24 09:45:05 +0530

    Display name, address from app.component.ts or typescript changes.

--- angular2/001-angular2quickstart/src/app/app.component.ts
+++ angular2/001-angular2quickstart/src/app/app.component.ts
@@ -2,6 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>Hello {{name}}</h1>
+  <p><strong>Email:</strong> {{email}}</p>
+  <p><strong>Address:</strong> {{address.street}}, {{address.city}}, {{address.state}}</p>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  name = 'Pruthvi Raju';
+  email = 'pruthvi@mail.com';
+  address = {
+    street: 'Road # 5',
+    city: 'Hyderabad',
+    state: 'TG'
+  }
+}
